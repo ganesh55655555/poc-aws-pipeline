3a70d7d703529ae3ed20c6ab43cc99becf132926
ganesh55655555 <151905631+ganesh55655555@users.noreply.github.com>
2026-01-02 21:39:13 +0530

    Update and rename validate.sh to cleanup.sh

---
+++ scripts/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+echo "Cleaning old application files..."
+rm -rf /var/www/html/*
--- scripts/validate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Validating application..."
-
-curl -f http://localhost/index.html
-
-echo "Validation successful"
