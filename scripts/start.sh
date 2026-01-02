468540c3d3d5b6cd8f70664a7841a0718f32cae1
ganesh55655555 <151905631+ganesh55655555@users.noreply.github.com>
2026-01-02 18:59:57 +0530

    Update and rename cleanup.sh to start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Setting permissions..."
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+echo "Starting Apache..."
+systemctl start apache2
+
+systemctl status apache2 --no-pager
